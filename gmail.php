d33cd72cf9bcdbccc1e38b32148198a3c7ee0fca
Shivam-Vishwakarma123 <69378491+Shivam-Vishwakarma123@users.noreply.github.com>
2020-08-12 12:06:37 +0530

    Alok

--- gmail.php
+++ gmail.php
@@ -11,6 +11,6 @@ $to = "vishwakarma888115@gmail.com";
 $headers = "Form: $email_form \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 mail($to, $email_subject, $email_body, $headers);
-header("Location: index.html");
+header("location: index.html");
 
 ?>
