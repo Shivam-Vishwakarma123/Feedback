24fa3bfa825b9fa9ba00c31801f50bb6ea35637b
Shivam-Vishwakarma123 <69378491+Shivam-Vishwakarma123@users.noreply.github.com>
2020-08-12 12:19:50 +0530

    Alok

--- gmail.php
+++ gmail.php
@@ -4,13 +4,13 @@ $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
-$email_form = 'vishwakarma@shivam.com';
+$email_from = 'vishwakarma@shivam.com';
 $email_subject = "New Form Submission";
 $email_body = "User Name: $name.\n"."User Email: $visitor_email.\n"."User Message: $message.\n";
 $to = "vishwakarma888115@gmail.com";
-$headers = "Form: $email_form \r\n";
+$headers = "Form: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 mail($to, $email_subject, $email_body, $headers);
-header("location: index.html");
+header("Location: index.html");
 
 ?>
