095680b5ecbb41eb726db6f2afd2f2dd1153b4c3
Shivam-Vishwakarma123 <69378491+Shivam-Vishwakarma123@users.noreply.github.com>
2020-08-12 12:21:37 +0530

    Update gmail.php

--- gmail.php
+++ gmail.php
@@ -9,7 +9,7 @@ $email_subject = "New Form Submission";
 $email_body = "User Name: $name.\n"."User Email: $visitor_email.\n"."User Message: $message.\n";
 $to = "vishwakarma888115@gmail.com";
 $headers = "Form: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
+$headers = "Reply-To: $visitor_email \r\n";
 mail($to, $email_subject, $email_body, $headers);
 header("Location: index.html");
 
